9f8efee41316a329ac765b3ea9ca6cc7b8e16eb4
mark <mark.kurman@gmail.com>
2023-05-11 23:16:27 +0300

    started workin on bow crud.. thoughts about making the list into a table with two columns.. name and subtotal

--- src/sections/overview/category-list.js
+++ src/sections/overview/category-list.js
@@ -17,16 +17,67 @@ import {
   SvgIcon
 } from '@mui/material';
 
+import * as React from 'react';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
+function FormDialog(props) {
+const { open, handleClose, t } = props;
+  return (
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>{t}</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            bow words go here?
+          </DialogContentText>
+          <TextField
+            autoFocus
+            margin="normal"
+            id="name"
+            label="type the words.."
+            type="text"
+            fullWidth
+            variant="standard"
+            multiline="true"
+            size='medium'
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={handleClose}>Update New Words</Button>
+        </DialogActions>
+      </Dialog>
+  );
+}
+
 export const Categories = (props) => {
   const { categories = [], sx, getDataClick } = props;
+  const [open, setOpen] = React.useState(false);
+  const [theTittle, setTheTittle] = React.useState("");
 
+  const handleClickOpen = (event, newTittle) => {
+    setOpen(true);
+    setTheTittle(newTittle);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  let counter = 1;
+  const handle = (event, param) => {
+    alert(param);
+  };
   return (
     <Card sx={sx}>
       <CardHeader title="My Categories" />
       <List>
         {categories.map((category, index) => {
           const hasDivider = index < categories.length - 1;
-
           return (
             <ListItem
               divider={hasDivider}
@@ -36,11 +87,17 @@ export const Categories = (props) => {
                 primary={category.category}
                 primaryTypographyProps={{ variant: 'subtitle1' }}
               />
-              <IconButton edge="end">
+              <IconButton edge="end" onClick={() => handleClickOpen(event, category.category)}>
                 <SvgIcon>
                   <EllipsisVerticalIcon />
+
                 </SvgIcon>
               </IconButton>
+              <FormDialog
+              open={open}
+              handleClose={handleClose}
+              t = {theTittle}
+            />
             </ListItem>
           );
         })}
